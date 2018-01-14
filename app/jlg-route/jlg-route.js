323b2c846a06acfd05608e2cb860660581f7b590
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-01-14 20:51:52 +0100

    ok

--- app/jlg-route/jlg-route.js
+++ app/jlg-route/jlg-route.js
@@ -62,5 +62,6 @@ app.run(($transitions) => {
 	$transitions.onSuccess({}, function(transition) {
 		angular.element(document.body).removeClass(transition.from().name);
 		angular.element(document.body).addClass(transition.to().name);
+		document.body.scrollTop = document.documentElement.scrollTop = 0;
 	});
 });
