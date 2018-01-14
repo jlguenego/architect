f9ec8b7147470089b0e1de327cf3cede7435880a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-01-14 20:18:09 +0100

    realisation

--- app/jlg-realisation/jlg-realisation.config.js
+++ app/jlg-realisation/jlg-realisation.config.js
@@ -8,15 +8,31 @@ export const config = ($stateProvider) => {
 		url: '/realisations/chauconin',
 		template: realisationHtml,
 		controller: function($state) {
+			this.data = $state.$current.data;
+			console.log('data', this.data);
 
 		},
+		controllerAs: '$ctrl',
 		data: {
-			title: `CADG: nos réalisations.`,
+			title: `Eglise Saint Saturnin de Chauconin`,
 			description: `
-Cabinet d'architecture DEMETRESCU - GUENEGO: liste de nos réalisations.
-Projet architecturaux réalisé par nos architectes du patrimoine.
-Eglises, Chateaux, Granges, Ecoles, Remparts, Lavoir, Mairies,
-Logements collectifs, Promotions immobilières, maisons individuelles`,
+this is the meta description...`,
+			image: {
+				main: 'img/photo/realisations/chauconin/saint-saturnin.jpg',
+				others: [
+					'img/photo/realisations/chauconin/saint-saturnin.jpg',
+					'img/photo/realisations/chauconin/saint-saturnin.jpg',
+				]
+			},
+			client: {
+				name: 'Mairie de Chauconin-Neufmontiers (77)',
+				url: 'http://chauconin-neufmontiers.fr/',
+			},
+			text: `
+			« L’originalité de votre église SAINT-SATURNIN consiste dans la conception architecturale qui réunit une période charnière de l’histoire : un passage harmonieux de l’architecture gothique à la resplendissante Renaissance française.
+			Construite à partir de 1580, elle se présente sous la forme d’une large nef unique à cinq travées terminées par une abside arrondie. Deux chapelles, l’une au nord, l’autre au sud donnent au plan un aspect cruciforme. Le transept est englobé complètement dans l’unité spatiale de la nef. A l’intérieur de l’église, les formes très épurées des murs sont animées par la lumière naturelle des fenêtres très larges qui occupent plus de la moitié des parois de chaque travée, lumière qui affine et dématérialise les colonnes engagées. Cette ambiance qui résulte du jeu subtil entre le «solide» et le «diaphane» est accompagnée d’une parfaite acoustique qui renforce le caractère majestueux de l’édifice. »
+			Suzana Guénégo, architecte 
+				`,
 		}
 	});
 
