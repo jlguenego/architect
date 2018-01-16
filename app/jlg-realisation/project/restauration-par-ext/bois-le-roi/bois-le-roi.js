d19c6e4e23b8bd8ecdfe6a3d3125fc0371aeaf94
dphengsiaroun <dphengsiaroun@outlook.fr>
2018-01-16 15:45:47 +0100

    add: lavoir

---
+++ app/jlg-realisation/project/restauration-par-ext/bois-le-roi/bois-le-roi.js
@@ -0,0 +1,38 @@
+
+import textHtml from './text.html';
+
+export const state = {
+	data: {
+		title: `Mairie de Bois le Roi`,
+		description: `Rénovation extérieure et intérieure Mairie à Bois le Roi`,
+		image: {
+			thumbnail: 'img/photo/project/lavoir/torcy/lavoir.jpg',
+			main: 'img/photo/project/lavoir/torcy/lavoir.jpg',
+			others: [
+				'img/photo/project/lavoir/torcy/lavoir.jpg',
+				'img/photo/project/lavoir/torcy/lavoir.jpg',
+			]
+		},
+		moa: {
+			name: 'Ville de Torcy (77)',
+			url: 'http://www.ville-torcy.fr/',
+		},
+		interventions: [
+			{
+				year: '2009',
+				title: `Restauration du lavoir communal`,
+				missions: [
+					'Etudes',
+					'Autorisation de travaux'
+				],
+			}
+		],
+		text: textHtml,
+	}
+};
+
+Rénovation extérieure et intérieure Mairie à Bois le Roi (77) 
+Maîtrise d’ouvrage : Ville de Bois le Roi (77)
+2017
+Mission de Maîtrise d’œuvre / Montant des travaux : 350 000 € HT
+
