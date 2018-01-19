68ffa86ad8fe7e4962f0856872469cfa03e6e099
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-01-19 17:47:50 +0100

    ok

--- sandbox/seo.js
+++ sandbox/seo.js
@@ -2,29 +2,31 @@ const HCCrawler = require('headless-chrome-crawler');
 const fs = require('fs');
 const path = require('path');
 const mkdirp = require('mkdirp');
-    
+
 
 
 const url = 'http://localhost:8000/app/';
 
 HCCrawler.launch({
 		// Function to be evaluated in browsers
-		evaluatePage: (() => ({
-			dom: document.children[0].innerHTML,
-		})),
+		evaluatePage: () => {
+			return {
+				dom: document.children[0].outerHTML,
+			};
+		},
 		// Function to be called with evaluated results from browsers
 		onSuccess: (result => {
 			console.log('toto', result);
 			let suffix = result.response.url.substring(url.length);
 			console.log('suffix', suffix);
-            suffix = (suffix === '') ? 'index.html' : suffix + '.html';
-            
-            const filename = path.resolve(__dirname, `../tmp/${suffix}`);
-            console.log('filename', filename);
+			suffix = (suffix === '') ? 'index.html' : suffix + '.html';
 
-           
-            
-            mkdirp.sync(path.dirname(filename));
+			const filename = path.resolve(__dirname, `../crawler-prerender/${suffix}`);
+			console.log('filename', filename);
+
+
+
+			mkdirp.sync(path.dirname(filename));
 
 			fs.writeFileSync(filename, result.result.dom);
 		}),
