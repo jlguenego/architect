a02739056437f46e7fc6cbf33852733af03aa9b3
dphengsiaroun <dphengsiaroun@outlook.fr>
2018-01-23 17:05:32 +0100

    ok

---
+++ app/jlg-realisation/project/scolaire/dammarie-les-lys2/dammarie-les-lys2.js
@@ -0,0 +1,32 @@
+import textHtml from './text.html';
+
+export const state = {
+	data: {
+		title: `Espace Jeunes à Dammarie les Lys`,
+		description: `espace Jeunes à Dammarie les Lys`,
+		image: {
+			main: 'img/photo/project/scolaire/dammarie-les-lys/espace-jeunes.jpg',
+			mainPortrait: 'img/photo/project/scolaire/dammarie-les-lys/espace-jeunes.jpg',
+			others: [
+				'img/photo/project/scolaire/dammarie-les-lys/espace-jeunes.jpg',
+				'img/photo/project/scolaire/dammarie-les-lys/espace-jeunes.jpg',
+			]
+		},
+		moa: {
+			name: 'Ville de Dammarie les Lys (77)',
+			url: 'http://www.mairie-dammarie-les-lys.fr/',
+		},
+		interventions: [
+			{
+				year: 2012,
+				title: `Aménagement espace Jeunes`,
+				missions: [
+					'Maîtrise d’œuvre',
+					'OPC'
+				],
+				price: '620 000 € HT'
+			}
+		],
+		text: textHtml,
+	}
+};
