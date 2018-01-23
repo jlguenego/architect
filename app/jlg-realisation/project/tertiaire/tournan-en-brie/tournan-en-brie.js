550ee8e7aa2a3c9f8078c16a181b74081ca10506
dphengsiaroun <dphengsiaroun@outlook.fr>
2018-01-23 17:25:02 +0100

    ok

---
+++ app/jlg-realisation/project/tertiaire/tournan-en-brie/tournan-en-brie.js
@@ -0,0 +1,32 @@
+import textHtml from './text.html';
+
+export const state = {
+	data: {
+		title: `Locaux administratifs à Tournan-en-Brie`,
+		description: `Réhabilitation d’une ancienne grange en locaux administratifs à Tournan-en-Brie`,
+		image: {
+			main: 'img/photo/project/tertiaire/tournan-en-brie/tertiaire.jpg',
+			mainPortrait: 'img/photo/project/tertiaire/tournan-en-brie/tertiaire.jpg',
+			others: [
+				'img/photo/project/tertiaire/tournan-en-brie/tertiaire.jpg',
+				'img/photo/project/tertiaire/tournan-en-brie/tertiaire.jpg',
+			]
+		},
+		moa: {
+			name: 'Ville de Tournan-en-Brie (77)',
+			url: 'http://www.tournan-en-brie.fr/',
+		},
+		interventions: [
+			{
+				year: '2017 – en cours',
+				title:'Réhabilitation d’une ancienne grange en locaux administratifs',
+				missions: [
+					'Maîtrise d\'oeuvre'
+				],
+				price: '741 900 € HT'
+			}
+		],
+		text: textHtml,
+	}
+};
+
