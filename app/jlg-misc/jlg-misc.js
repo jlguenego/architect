6b4e429cb8b90f6153972b662be6e51ef5085b96
dphengsiaroun <dphengsiaroun@outlook.fr>
2018-02-01 16:24:06 +0100

    toslug working

--- app/jlg-misc/jlg-misc.js
+++ app/jlg-misc/jlg-misc.js
@@ -3,6 +3,6 @@ const app = angular.module('jlg-misc', []);
 app.service('misc', function Misc() {
 	this.toSlug = function(text) {
 		return text.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
-			.replace(/ /g, '-').replace(/[^a-zA-Z0-9-]/g, '').toLowerCase();
+			.replace(/[ ']/g, '-').replace(/[^a-zA-Z0-9-]/g, '').toLowerCase();
 	};
 });
