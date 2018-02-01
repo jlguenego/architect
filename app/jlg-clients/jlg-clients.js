4d065576edf6264226db4bfc87b7772762b424e9
dphengsiaroun <dphengsiaroun@outlook.fr>
2018-02-01 15:29:23 +0100

    ok

--- app/jlg-clients/jlg-clients.js
+++ app/jlg-clients/jlg-clients.js
@@ -2,12 +2,18 @@ const app = angular.module('jlg-clients', []);
 
 app.controller('ClientCtrl', function ClientCtrl(projects) {
 	'ngInject';
-	this.list = projects.filter(state => typeof state.data.client === 'object').map(state => {
-		console.log('state', state);
+	this.list = projects.filter(state => typeof state.data.client === 'object').reduce((acc, state) => {
+		console.log('state, acc', state, acc);
 		const years = state.data.interventions.map(n => n.year).sort();
+		const duplicate = acc.find(client => client.name === state.data.client.name);
+		if (duplicate) {
+			duplicate.year += ', ' + years.join(', ');
+			return acc;
+		}
 		state.data.client.year = years.join(', ');
-		return state.data.client;
-	});
+		acc.push(state.data.client);
+		return acc;
+	}, []);
 
 	this.mairies = this.list.filter(n => n.type === 'Mairie');
 	this.autresEntitesPubliques = this.list.filter(n => n.type === 'Public Autres');
