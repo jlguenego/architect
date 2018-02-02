8d476fb55dd6891d809d8f33f4f25754c4bccf26
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-02-02 18:59:26 +0100

    poincy

--- app/jlg-realisation/project/scolaire/poincy/poincy.js
+++ app/jlg-realisation/project/scolaire/poincy/poincy.js
@@ -5,18 +5,30 @@ export const state = {
 		title: `Ecole de Poincy`,
 		description: `Réhabilitation de l’école à Poincy`,
 		image: {
-			main: 'img/photo/project/scolaire/poincy/ecole.jpg',
-			mainPortrait: 'img/photo/project/scolaire/poincy/ecole.jpg',
+			main: 'img/photo/project/scolaire/poincy/parallax.jpg',
+			mainPortrait: 'img/photo/project/scolaire/poincy/parallax-portrait.jpg',
 			trios: [
 				[{
 					url: 'img/photo/project/scolaire/poincy/01.jpg',
-					caption: `Façade arrière de l'école`
+					caption: 'Façade principale - après travaux'
 				}, {
 					url: 'img/photo/project/scolaire/poincy/02.jpg',
-					caption: 'Avant travaux'
+					caption: 'Façade arrière - après travaux'
 				}, {
 					url: 'img/photo/project/scolaire/poincy/03.jpg',
-					caption: 'Après travaux'
+					caption: `Intérieur - après travaux`
+				}],
+				[{
+					url: 'img/photo/project/scolaire/poincy/04.jpg',
+					caption: 'Façade principale - avant travaux'
+					
+				}, {
+					url: 'img/photo/project/scolaire/poincy/05.jpg',
+					caption: `Façade arrière - avant travaux`
+					
+				}, {
+					url: 'img/photo/project/scolaire/poincy/06.jpg',
+					caption: 'Intérieur - avant travaux'
 				}]
 			]
 		},
