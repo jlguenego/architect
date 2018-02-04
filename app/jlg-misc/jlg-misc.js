3f0da54036e43590accd4aea13863a5fb4f5869d
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-02-04 10:47:18 +0100

    work on safari iphone 5

--- app/jlg-misc/jlg-misc.js
+++ app/jlg-misc/jlg-misc.js
@@ -2,7 +2,13 @@ const app = angular.module('jlg-misc', []);
 
 app.service('misc', function Misc() {
 	this.toSlug = function(text) {
-		return text.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
+		if (String.prototype.normalize) {
+			text = text.normalize('NFD');
+		}
+
+		const result = text.replace(/[\u0300-\u036f]/g, '')
 			.replace(/[ ']/g, '-').replace(/[^a-zA-Z0-9-]/g, '').toLowerCase();
+
+		return result;
 	};
 });
