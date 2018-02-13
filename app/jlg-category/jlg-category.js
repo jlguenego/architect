154eb8d6b35ec7104081c2575738da8b37bef351
dphengsiaroun <dphengsiaroun@outlook.fr>
2018-02-13 12:01:26 +0100

    add: thumbnail category

--- app/jlg-category/jlg-category.js
+++ app/jlg-category/jlg-category.js
@@ -3,44 +3,54 @@ const app = angular.module('jlg-category', []);
 const chapterMap = [{
 	key: 'chateau',
 	value: 'Châteaux',
+	image: 'thumbnail-ferrieres',
 }, {
 	key: 'eglise',
 	value: 'Eglises',
 	description: `
     Au moins 40 mairies du département de Seine-et-Marne nous ont fait confiance
     pour intervenir sur leur églises (Etude préalable, Travaux, etc.)           
-    `
+	`,
+	image: 'thumbnail-ferrieres',
 }, {
 	key: 'rempart',
 	value: 'Remparts',
+	image: 'thumbnail-ferrieres',
 }, {
 	key: 'abbaye',
 	value: 'Abbayes',
+	image: 'thumbnail-ferrieres',
 }, {
 	key: 'ferme',
 	value: 'Fermes',
+	image: 'thumbnail-ferrieres',
 }, {
 	key: 'restauration-par-ext',
 	value: 'Restauration - Parements extérieurs',
+	image: 'thumbnail-ferrieres',
 }, {
 	key: 'serre',
 	value: 'Serres',
+	image: 'thumbnail-ferrieres',
 }, {
 	key: 'tertiaire',
 	value: 'Tertiaire - Administration - Autres - Public',
+	image: 'thumbnail-ferrieres',
 }, {
 	key: 'sport',
 	value: 'Sports - Loisirs',
+	image: 'thumbnail-ferrieres',
 }, {
 	key: 'marche-couvert',
 	value: 'Marchés couverts',
+	image: 'thumbnail-ferrieres',
 }, {
 	key: 'scolaire',
 	value: 'Scolaire - Education',
+	image: 'thumbnail-ferrieres',
 }];
 
 chapterMap.forEach(n => {
-	n.image = `img/photo/project/${n.key}/thumbnail-${n.key}.jpg`;
 	n.state = `realisations:category({category: '${n.key}'})`;
 });
 
